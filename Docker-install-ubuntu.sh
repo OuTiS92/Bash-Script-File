bb1a7cc0abd5bdcc1d1d878d432ea4ccc221d613
OuTiS <33751897+OuTiS92@users.noreply.github.com>
2023-09-17 19:28:24 +0330

    Update Docker-install-ubuntu.sh

--- Docker-install-ubuntu.sh
+++ Docker-install-ubuntu.sh
@@ -9,5 +9,23 @@ EOF
 
 slpee 2 
 clear
+echo "if you from iran , type iran ya age zendegi nimikonid dar iran type kondi no "
+sleep 2
+read -p " where are you ???"  where
 
-read -p " where are you ???" 
+case $where in 
+  [iran] )  echo "OK nedd nameserver .... Please ...";
+        echo "name serverr 10.202.10.202 > /etc/resolv.conf ";
+        echo "mirim baraye nasb docker ... "
+        sleep 2 
+          clear 
+
+        
+          break;;
+  [no] ) echo "exiting ....";
+      exit;;
+    * ) echo "invalid command ....";;
+esac 
+done 
+
+
