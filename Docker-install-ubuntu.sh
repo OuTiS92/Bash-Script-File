62f114e861bddc76c269b34d3a6fa5978472eda9
outis <amirhoseinalahdad76@gmail.com>
2023-09-17 20:37:58 +0330

    Version 1

--- Docker-install-ubuntu.sh
+++ Docker-install-ubuntu.sh
@@ -17,6 +17,36 @@ case $where in
         echo -e "nameserverr 10.202.10.202\nnameserver 10.202.10.102" > /etc/resolv.conf
         echo "mirim baraye nasb docker ... "
         sleep 2 
+	sudo apt update
+	wait 
+	sudo apt remove docker*
+	wait
+	clear
+	sleep 1
+	sudo apt install apt-transport-https ca-certificates curl software-properties-common
+	wait 
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	wait
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+	wait
+	apt-cache policy docker-ce
+	wait
+	sudo apt install docker-ce
+	wait
+	service_name="docker"
+	if systemctl is-active --quiet "$service_name"; then 
+		echo "$service_name  runnig ... "
+	else 
+		systemctl start "$service_name"
+	fi
+	sudo usermod -aG docker ${USER}
+	su - ${USER}
+	sleep 1 
+	clear 
+
+	echo "Docker Install .... GG"
+	
+	
 
           clear 
 
@@ -26,6 +56,5 @@ case $where in
       exit;;
     * ) echo "invalid command ....";;
 esac 
-done 
 
 
